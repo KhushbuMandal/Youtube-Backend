bac6009fb599fe366889dbde3c2d20087d47942b
KhushbuMandal <khushbumandal4459@gmail.com>
2024-08-05 19:55:41 +0530

    Learnt about Cors and cookie

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@
 
 import dotenv from "dotenv"
 import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './.env'
+    path: './env'
 })
 
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 5000 , () => {
+        console.log(`Server is running at poirt : ${process.env.PORT}`);
+    })
+
+})
+.catch((err) => {
+    console.log ("MONGODB db connection failed !!! " , err);
+})
 
 
 
