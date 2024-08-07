89c64cbe01f3a43759efe6a92cdd08b1a85c9ce0
KhushbuMandal <khushbumandal4459@gmail.com>
2024-08-08 01:09:58 +0530

    🌱 Declare the register route for registration

--- src/app.js
+++ src/app.js
@@ -15,5 +15,15 @@ app.use (express.static("public"))
 app.use (cookieParser())
 
 
+// routes import 
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+//this will work as http://localhost:8000/api/v1/users/register -> here /api/v1/users act as middleware
+app.use("/api/v1/users" , userRouter)
+
+
+
 
 export { app };
