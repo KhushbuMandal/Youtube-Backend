f3f0fe9eab6b08f32a68e28def63ee1d7eeb21fc
KhushbuMandal <khushbumandal4459@gmail.com>
2024-08-11 23:07:41 +0530

    ^o^ Added Subsciption model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,8 @@ import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
-import jwt, { verify } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+
 
 
 /*
