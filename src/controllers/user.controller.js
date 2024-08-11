158d3777554a4d586fb0e3a402f4dbc4fed833a1
KhushbuMandal <khushbumandal4459@gmail.com>
2024-08-12 00:27:20 +0530

    🚀Added Update Account Details lofic in Controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -354,9 +354,47 @@ const getCurrentUser  = asyncHandler (async (req,res) => {
     .json(200 , user , "Current user fetced successfully")
 })
 
+
+/*
+  * / / / / / / / / / / / / / /
+ * update account details
+ *  / / / / / / / / / / / / / /
+ */
+
+const updateAccountDetails = asyncHandler ( async (req, res) => {
+    const {fullname , email} = req.body;
+
+    if(!fullname || !email){
+        throw new ApiError(400,"All fields are required")
+    }
+
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullname,
+                email : email
+            }
+        },
+        {
+            new : true
+        }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user,"Account details updated successfully"))
+
+
+})
+
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
 }
